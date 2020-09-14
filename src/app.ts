5376f15a9ccb8ce422a2ec40bb64aade97102b59
olu <ebenezerajayi@gmail.com>
2020-09-14 03:55:24 +0100

    reusable validation using Interface

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
+//  Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean; // the ? allows this value to be optional, you could also do boolean | undefined
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(input: Validatable) {
+  let isValid = true;
+  if(input.required) {
+    isValid = isValid && input.value.toString().trim().length !== 0
+  }
+  if(input.minLength != null && typeof input.value === 'string'){
+    isValid = isValid && input.value.length >= input.minLength;
+  }
+  if(input.maxLength != null && typeof input.value === 'string'){
+    isValid = isValid && input.value.length <= input.maxLength;
+  }
+  if(input.min != null && typeof input.value === 'number') {
+    isValid = isValid && input.value >= input.min
+  }
+  if(input.max != null && typeof input.value === 'number') {
+    isValid = isValid && input.value <= input.max
+  }
+  return isValid;
+}
+
+
 //  autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -39,8 +70,30 @@ class ProjectInput {
     const description = this.descriptionInputElement.value;
     const people = this.peopleInputElement.value;
 
-    if(title.trim().length === 0 || description.trim().length === 0 ||people.trim().length === 0) {
-      alert('no epmty space allowed');
+    const titleValidatable: Validatable = {
+      value: title,
+      required: true,
+      minLength:2
+    }
+
+    const descriptionValidatable: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5
+    }
+
+    const peopleValidatable: Validatable = {
+      value: +people,
+      required: true,
+      min: 1,
+      max:5
+    }
+
+    if(!validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('please enter valid data');
       return;
     }
     return [title, description, +people]
