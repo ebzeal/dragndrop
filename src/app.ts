c76c9a891eb2910c3434a8a5eff472805fe5fbec
olu <ebenezerajayi@gmail.com>
2020-09-21 01:19:47 +0100

    fix duplictaion on active and inactive projects

--- src/app.ts
+++ src/app.ts
@@ -105,7 +105,13 @@ class ProjectList {
   this.listElement.id = `${this.type}-projects`;
   
   projectState.addListener((projects: Project[])=>{
-    this.assignedProjects = projects;
+    const relevantProjects = projects.filter(project => {
+      if(this.type === 'active'){
+      return project.status === ProjectStatus.Active;
+    }
+     return project.status === ProjectStatus.Finished
+    })
+    this.assignedProjects = relevantProjects;
     this.renderProjects();
   });
 
@@ -115,6 +121,7 @@ class ProjectList {
 
  private renderProjects() {
    const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+   listEl.innerHTML ='';
    for (const item of this.assignedProjects) {
     const listItem = document.createElement('li');
     listItem.textContent = item.title;
